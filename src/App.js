9db00f3ee1d723d64023de341be6802e6a515255
carmenlgv <carmety11@hotmail.com>
2020-05-21 21:57:41 -0500

    Mejoras en reglas

--- src/App.js
+++ src/App.js
@@ -27,44 +27,39 @@ class App extends Component {
   }
 
   seleccionarVecinos() {
-    let estadoSig;
     let celulasTemp=[];
-    let celulas = this.state.arreglo;
     let vecino;
-    celulas.map((celula) => {
-      let coordX = 0;
-      let coordY = 0;
+    this.state.arreglo.map((celula) => {
+      let coordX;
+      let coordY;
       let numVecinos = 0;
       for (let x = -1; x <= 1; x++) {
         for (let y = -1; y <= 1; y++) {
           if (x !== 0 || y !== 0) {
             coordX = (celula.x + x + this.filas) % this.filas;
             coordY = (celula.y + y + this.filas) % this.filas;
-            vecino = celulas.find(x => x.id === coordX + "." + coordY);
+            vecino = this.state.arreglo.find(x => x.id === coordX + "." + coordY);
             numVecinos += vecino.estado;
           }
         }
       }
-      estadoSig=this.reglas(celula.estado,numVecinos);
-      celula.estadoSig=estadoSig;
+      celula.estadoSig=(numVecinos === 2 || numVecinos === 3)?this.reglas(celula.estado,numVecinos):0;
     })
-    celulasTemp=celulas;
+    celulasTemp=this.state.arreglo;
     return(this.cambioDeEstado(celulasTemp));
   }
 
   reglas(estado,numVecinos) {
-    let estadoSig=0;
-      if (estado === 0) {
-        if (numVecinos === 3) {
-          estadoSig = 1;
-        }
-      }
-      else {
-        if (numVecinos === 2 || numVecinos === 3) {
-          estadoSig = 1;
-        }
-      }
+    let estadoSig = (estado === 0)?this.celulaMuerta(numVecinos):this.celulaViva(numVecinos);
       return estadoSig;
+  }
+  celulaMuerta(numVecinos){
+    let estadoSig = (numVecinos === 3)?1:0;
+    return estadoSig;
+  }
+  celulaViva(numVecinos){
+    let estadoSig = (numVecinos === 2 || numVecinos === 3)?1:0;
+    return estadoSig;
   }
   cambioDeEstado(celulasTemp){
     celulasTemp.map((celula) => {  
@@ -82,7 +77,4 @@ class App extends Component {
     );
   }
 }
-
-
-
 export default App;
