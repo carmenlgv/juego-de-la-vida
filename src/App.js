4bb057ddadde6123b7b34d989dd8c49f6944da5e
carmenlgv <carmety11@hotmail.com>
2020-05-21 23:30:27 -0500

    Mejoras en calculo de vecinos

--- src/App.js
+++ src/App.js
@@ -17,6 +17,7 @@ class App extends Component {
     this.state = estadoInicial(this.filas);
   }
   tick() {
+    //El state se cambia al arreglo temporal
     this.setState(this.seleccionarVecinos());
   }
   componentDidMount() {
@@ -27,42 +28,47 @@ class App extends Component {
   }
 
   seleccionarVecinos() {
-    let celulasTemp=[];
-    let vecino;
+    //Por cada celula se obtiene el número de vecinos vivos que tiene
+    //Si el número de vecinos es igual a 3, en la siguiente generación vivirá, sin importar el estado.
+    //Regresa un arreglo temporal en donde guarda el siguiente estado
+    let celulasTemp = [];
+    let numVecinos;
     this.state.arreglo.map((celula) => {
-      let coordX;
-      let coordY;
-      let numVecinos = 0;
-      for (let x = -1; x <= 1; x++) {
-        for (let y = -1; y <= 1; y++) {
-          if (x !== 0 || y !== 0) {
-            coordX = (celula.x + x + this.filas) % this.filas;
-            coordY = (celula.y + y + this.filas) % this.filas;
-            vecino = this.state.arreglo.find(x => x.id === coordX + "." + coordY);
-            numVecinos += vecino.estado;
-          }
-        }
-      }
-      celula.estadoSig=(numVecinos === 2 || numVecinos === 3)?this.reglas(celula.estado,numVecinos):0;
+      numVecinos = this.calculoVecinos(celula);
+      celula.estadoSig = (numVecinos === 3) ? 1 : this.reglas(celula.estado, numVecinos);
     })
-    celulasTemp=this.state.arreglo;
-    return(this.cambioDeEstado(celulasTemp));
+    celulasTemp = this.state.arreglo;
+    return (this.cambioDeEstado(celulasTemp));
   }
 
-  reglas(estado,numVecinos) {
-    let estadoSig = (estado === 0)?this.celulaMuerta(numVecinos):this.celulaViva(numVecinos);
-      return estadoSig;
+  calculoVecinos(celula) {
+    //Calculo de cuántos vecinos vivos tiene una celula
+    let numVecinos = 0;
+    for (let x = -1; x <= 1; x++) {
+      for (let y = -1; y <= 1; y++) {
+        numVecinos += (x !== 0 || y !== 0) ? this.estadoVecino(celula.x + x, celula.y + y) : 0;
+      }
+    }
+    return numVecinos;
   }
-  celulaMuerta(numVecinos){
-    let estadoSig = (numVecinos === 3)?1:0;
+  estadoVecino(x, y) {
+    //Regresa el estado de cada vecino
+    let vecino;
+    x = (x + this.filas) % this.filas;
+    y = (y + this.filas) % this.filas;
+    vecino = this.state.arreglo.find(z => z.id === x + "." + y);
+    return vecino.estado;
+  }
+
+  reglas(estado, numVecinos) {
+    //Si la celula está viva y si tiene 2 vecinos, vivirá
+    let estadoSig = (estado === 1 && numVecinos === 2) ? 1 : 0;
     return estadoSig;
   }
-  celulaViva(numVecinos){
-    let estadoSig = (numVecinos === 2 || numVecinos === 3)?1:0;
-    return estadoSig;
-  }
-  cambioDeEstado(celulasTemp){
-    celulasTemp.map((celula) => {  
+  cambioDeEstado(celulasTemp) {
+    //El estado que tendrá la celula en la siguiente generación
+    //se cambia al atributo estado para que pueda ser reconocido
+    celulasTemp.map((celula) => {
       celula.estado = celula.estadoSig;
     })
     return celulasTemp;
@@ -72,7 +78,7 @@ class App extends Component {
       <div className="App">
         <Tablero
           arreglo={this.state.arreglo}
-        />         
+        />
       </div>
     );
   }
